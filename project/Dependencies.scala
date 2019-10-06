d462c30399be469b10f29c348858674905adb041
Scala Steward <me@scala-steward.org>
2019-10-06 09:15:37 +0200

    Update zio-interop-cats to 2.0.0.0-RC5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsEffect = "2.0.0"
 
     val zio = "1.0.0-RC14"
-    val ziocats = "2.0.0.0-RC3"
+    val ziocats = "2.0.0.0-RC5"
 
     val logback = "1.2.3"
     val log4cats = "1.0.0"
