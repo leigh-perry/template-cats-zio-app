ad0145461fe5cf08ac12f52971d491e332199db8
leigh-perry <lperry.breakpoint@gmail.com>
2020-01-15 11:53:57 +1100

    Test name

--- modules/shared/src/test/scala/com/lptemplatecompany/lptemplatedivision/shared/AppsSpec.scala
+++ modules/shared/src/test/scala/com/lptemplatecompany/lptemplatedivision/shared/AppsSpec.scala
@@ -1,14 +1,12 @@
 package com.lptemplatecompany.lptemplatedivision.shared
 
 import com.lptemplatecompany.lptemplatedivision.shared.testsupport.BaseSpec
-import zio.random
 import zio.test.Assertion._
 import zio.test._
-import zio.test.environment.TestRandom
 
 object AppsSpec
   extends BaseSpec(
-    suite("AppsTest")(
+    suite("Apps")(
       test("className") {
         assert(Apps.className(Apps), equalTo("Apps"))
       }
