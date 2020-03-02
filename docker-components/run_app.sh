a9d49d810a111aeabe85e0187c74bb34db2952f4
leigh-perry <lperry.breakpoint@gmail.com>
2020-03-02 15:33:05 +1100

    Tidy docker run script

--- docker-components/run_app.sh
+++ docker-components/run_app.sh
@@ -2,23 +2,31 @@
 
 # This runs inside the application docker container, all at /app
 
-#JVM_SWITCHES="--add-modules java.xml.bind"
-#JVM_SWITCHES="-XshowSettings:vm -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1"
-JVM_SWITCHES="-XshowSettings:vm -XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport"
-SCALA_SWITCHES="-Dscala.concurrent.context.minThreads=4 -Dscala.concurrent.context.numThreads=x4 -Dscala.concurrent.context.maxThreads=64"
-JAVA_BASE_COMMAND="java -cp $(printf ":%s" lib/*.jar | cut -c2-) ${JVM_SWITCHES} ${SCALA_SWITCHES}"
+jarsPath () {
+  printf ":%s" /app/lib/*.jar | cut -c2-
+}
 
-echo ================================================================================
-ls -lR /app
-echo ================================================================================
-env | grep LPTEMPLATEENVPREFIX_ | sort
-echo ================================================================================
+main() {
+  JVM_SWITCHES="-XshowSettings:vm -XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport"
+  SCALA_SWITCHES="-Dscala.concurrent.context.minThreads=4 -Dscala.concurrent.context.numThreads=x4 -Dscala.concurrent.context.maxThreads=64"
 
-if [[ "$LPTEMPLATEENVPREFIX_OPERATION" == "lptemplateservicename" ]]; then
+  JARS="$(jarsPath)"
+  JAVA_BASE_COMMAND="java -cp ${JARS} ${JVM_SWITCHES} ${SCALA_SWITCHES}"
+
+  echo ================================================================================
+  ls -lR /app
+  echo ================================================================================
+  env | grep LPTEMPLATEENVPREFIX_ | sort
+  echo ================================================================================
+
+  if [[ "$LPTEMPLATEENVPREFIX_OPERATION" == "lptemplateservicename" ]]; then
     set -x
     exec ${JAVA_BASE_COMMAND} com.lptemplatecompany.lptemplatedivision.lptemplateservicename.AppMain
 
-else
-  echo "Unknown option '$LPTEMPLATEENVPREFIX_OPERATION'"
+  else
+    echo "Unknown option '$LPTEMPLATEENVPREFIX_OPERATION'"
 
-fi
+  fi
+}
+
+main "$@"
