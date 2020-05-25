df4a833749393571f7c0c84a9de4df7c21e32c27
Scala Steward <me@scala-steward.org>
2020-05-26 00:34:38 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.0-RC20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsEffect = "2.1.3"
     val catsMouse = "0.25"
 
-    val zio = "1.0.0-RC19-2"
+    val zio = "1.0.0-RC20"
     val zioConfig = "1.0.0-RC18"
     val zioCats = "2.0.0.0-RC14"
 
