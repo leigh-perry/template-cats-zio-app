914eec50646ddc7948158b34ac213dd0a0becff5
Scala Steward <me@scala-steward.org>
2020-05-29 10:23:43 +0200

    Update zio-config to 1.0.0-RC20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsMouse = "0.25"
 
     val zio = "1.0.0-RC20"
-    val zioConfig = "1.0.0-RC18"
+    val zioConfig = "1.0.0-RC20"
     val zioCats = "2.0.0.0-RC14"
 
     val logback = "1.2.3"
