46c808828ffd74eabfa72521cb99d34622903e49
Scala Steward <me@scala-steward.org>
2021-04-12 06:30:34 +0200

    Update zio, zio-test, zio-test-sbt to 1.0.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val catsEffect = "3.0.2"
     val catsMouse = "0.26.2"
 
-    val zio = "1.0.5"
+    val zio = "1.0.6"
     val zioConfig = "1.0.4"
     val zioCats = "2.4.0.0"
 
