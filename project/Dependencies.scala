f1fd458eed9beaddd11e1afe2efbe78abef98bdf
Scala Steward <me@scala-steward.org>
2021-05-13 21:50:55 +0200

    Update cats-core to 2.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object Version {
     val kindProjectorVersion = "0.10.3"
 
-    val cats = "2.6.0"
+    val cats = "2.6.1"
     val catsEffect = "3.1.0"
     val catsMouse = "1.0.2"
 
