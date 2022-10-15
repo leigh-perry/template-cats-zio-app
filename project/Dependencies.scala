0e07fd70f5b8bfd8e350dc69c99ba8a005f91db5
Scala Steward <scala_steward@virtuslab.com>
2022-10-15 00:38:28 +0000

    Update mouse to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     val cats = "2.6.1"
     val catsEffect = "3.1.1"
-    val catsMouse = "1.0.11"
+    val catsMouse = "1.2.0"
 
     val zio = "1.0.16"
     val zioConfig = "1.0.10"
